9259f500e6db3ac4cb1e2b28a1f39325ae4f7bfb
mOoF <32277303+mOoFmAf@users.noreply.github.com>
2025-01-14 19:30:36 -0500

    Update hello.cs

--- HelloWorld/hello.cs
+++ HelloWorld/hello.cs
@@ -1,10 +1,34 @@
 
 using System;
+using System.Windows.Forms;
 
-namespace hi {
+namespace Hi {
 	class Hello {
+
+		public static int square(int a) {
+			return a * a;
+		}
+
 		public static void Main(string[] args) {
-			Console.WriteLine("Hello, World");
+			Console.WriteLine("Hello, World: " + square(2));
+			square(100);
+			
+			Form form = new Form();
+			Button b = new Button()
+			{
+				Text = "Hello",
+				Location = new System.Drawing.Point(10, 10)
+			};
+			b.Click += (o, s) => {
+				MessageBox.Show("World");
+			};
+			
+			form.Controls.Add(b);
+			form.ShowDialog();
+			
+			while(form.Created) {
+				
+			}
 		}
 	}
 }
